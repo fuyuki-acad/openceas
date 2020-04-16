f9d7362effd48329b0701b3c54f01e5abf92909c
Takahiro Okuda <okuda@bownet.co.jp>
2020-04-16 15:19:07 +0900

    Issue #100

--- app/models/assignment_essay_comment_history.rb
+++ app/models/assignment_essay_comment_history.rb
@@ -1,3 +1,10 @@
 class AssignmentEssayCommentHistory < ApplicationRecord
+  include UploadFileModule
+
   belongs_to  :answer_score_history
+
+  def get_file_path
+    essay_path = self.get_essay_path(self.answer_score_history.generic_page.course)
+    essay_path + self.processed_link_name.to_s
+  end
 end
