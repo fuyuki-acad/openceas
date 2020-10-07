604544dbbed1397020a7174fd5bd88275e422ae5
Takahiro Okuda <okuda@bownet.co.jp>
2020-10-07 11:37:41 +0900

    Issue #175

---
+++ spec/controllers/compounds_controller_spec.rb
@@ -0,0 +1,354 @@
+require 'rails_helper'
+
+RSpec.describe CompoundsController, type: :controller do
+  let(:student) { create(:student_user) }
+
+  before do
+    @course = create(:course, :year_of_2019)
+
+    @class_session_no = 2
+
+    @class_session = create(:class_session, course_id: @course.id, class_session_no: @class_session_no)
+  end
+
+  describe '複合式テスト実施' do
+    context '生徒ユーザ' do
+      before do
+        sign_in student
+
+        create(:course_enrollment_user, course_id: @course.id, user_id: student.id)
+      end
+
+      context '未受験' do
+        context '期限前' do
+          before do
+            @compound = create(:compound, course_id: @course.id, start_time: Time.new + 1.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("error")
+          end
+        end
+
+        context '期限終了' do
+          before do
+            @compound = create(:compound, course_id: @course.id, end_time: Time.new - 1.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("error")
+          end
+        end
+
+        context '有効期間内' do
+          before do
+            @compound = create(:compound, course_id: @course.id, start_time: Time.new - 1.day, end_time: Time.new + 1.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("show")
+          end
+        end
+
+        context '開始パスワード有り' do
+          before do
+            @compound = create(:compound, course_id: @course.id, start_pass: "12345")
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("start_password")
+          end
+
+          it "パスワード誤り" do
+            post :password, params: { id: @compound.id, class_session_no: @class_session_no, start_pass: "11111" }
+
+            expect(response).to render_template("start_password")
+          end
+
+          it "パスワード一致" do
+            post :password, params: { id: @compound.id, class_session_no: @class_session_no, start_pass: "12345" }
+
+            expect(response).to render_template("show")
+          end
+        end
+
+        context '回答' do
+          before do
+            @compound = create(:compound, course_id: @course.id, start_time: Time.new - 1.day, end_time: Time.new + 1.day)
+            @parent_question = create(:parent_question)
+            @compound.questions << @parent_question
+            create_list(:question, 3, parent_question_id: @parent_question.id, generic_page_type_cd: @compound.type_cd, 
+              quizzes_attributes: {
+                "1" => { 'content' => "選択肢１", 'select_correct_flag' => "0" },
+                "2" => { 'content' => "選択肢２", 'select_correct_flag' => "0" },
+                "3" => { 'content' => "選択肢３", 'select_correct_flag' => "0" },
+                "4" => { 'content' => "選択肢４", 'select_correct_flag' => "1" }
+              }
+            )
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+          end
+
+          it "確認＆保存" do
+            compound_answers = {}
+            @parent_question.questions.each do |question|
+              compound_answers[question.id] = { select_answer_id: question.all_quizzes[0].id }
+            end
+
+            patch :confirm, params: { id: @compound.id, answers: compound_answers }
+            expect(response).to render_template("confirm")
+
+            expect{
+              patch :save, params: { id: @compound.id }
+            }.to change(AnswerScore, :count).by(1)
+
+            expect(response).to redirect_to action: :mark, id: @compound.id
+          end
+
+          it "一時保存後＆保存" do
+            compound_answers = {}
+            @parent_question.questions.each do |question|
+              compound_answers[question.id] = { select_answer_id: question.all_quizzes[0].id }
+            end
+
+            #一次保存
+            expect{
+              patch :save, params: { id: @compound.id, answers: compound_answers }
+            }.to change(AnswerScore, :count).by(1)
+
+            @compound.reload
+            expect(@compound.latest_score(student.id).total_score).to eq Settings.ANSWERSCORE_TMP_SAVED_SCORE
+            expect(response).to redirect_to compound_path(@compound)
+
+
+            @parent_question.questions.each do |question|
+              compound_answers[question.id] = { select_answer_id: question.all_quizzes[1].id }
+            end
+
+            #確認
+            patch :confirm, params: { id: @compound.id, answers: compound_answers }
+            expect(response).to render_template("confirm")
+
+            #保存
+            expect{
+              patch :save, params: { id: @compound.id }
+            }.to change(AnswerScore, :count).by(0)
+
+            @compound.reload
+            expect(@compound.latest_score(student.id).total_score).to_not eq Settings.ANSWERSCORE_TMP_SAVED_SCORE
+            expect(response).to redirect_to action: :mark, id: @compound.id
+          end
+        end
+
+        context '回答：期限切れ' do
+          before do
+            @compound = create(:compound, course_id: @course.id, start_time: Time.new - 1.day, end_time: Time.new - 1.day)
+            @parent_question = create(:parent_question)
+            @compound.questions << @parent_question
+            create_list(:question, 3, parent_question_id: @parent_question.id, generic_page_type_cd: @compound.type_cd, 
+              quizzes_attributes: {
+                "1" => { 'content' => "選択肢１", 'select_correct_flag' => "0" },
+                "2" => { 'content' => "選択肢２", 'select_correct_flag' => "0" },
+                "3" => { 'content' => "選択肢３", 'select_correct_flag' => "0" },
+                "4" => { 'content' => "選択肢４", 'select_correct_flag' => "1" }
+              }
+            )
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+          end
+
+          it "確認＆保存" do
+            compound_answers = {}
+            @parent_question.questions.each do |question|
+              compound_answers[question.id] = { select_answer_id: question.all_quizzes[0].id }
+            end
+
+            patch :confirm, params: { id: @compound.id, answers: compound_answers }
+            expect(response).to render_template("confirm")
+
+            expect{
+              patch :save, params: { id: @compound.id }
+            }.to change(AnswerScore, :count).by(0)
+
+            expect(response).to render_template("error")
+          end
+        end
+      end
+
+      context '1回受験済み：期限終了：最大受験回数１回' do
+        before do
+          @compound = create(:compound, course_id: @course.id, end_time: Time.new - 1.day)
+          parent_question = create(:parent_question)
+          @compound.questions << parent_question
+          create_list(:question, 3, parent_question_id: parent_question.id, generic_page_type_cd: @compound.type_cd, 
+            quizzes_attributes: {
+              "1" => { 'content' => "選択肢１", 'select_correct_flag' => "0" },
+              "2" => { 'content' => "選択肢２", 'select_correct_flag' => "0" },
+              "3" => { 'content' => "選択肢３", 'select_correct_flag' => "0" },
+              "4" => { 'content' => "選択肢４", 'select_correct_flag' => "1" }
+            }
+          )
+          create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+        end
+
+        context '期限終了：合格' do
+          before do
+            create(:passed_answer_score, page_id:@compound.id, user_id: student.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("mark")
+          end
+        end
+
+        context '期限終了：不合格' do
+          before do
+            create(:failed_answer_score, page_id:@compound.id, user_id: student.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("mark")
+          end
+        end
+      end
+
+      context '1回受験済み：期限終了：最大受験回数２回' do
+        before do
+          @compound = create(:compound, course_id: @course.id, end_time: Time.new - 1.day, max_count: 2)
+          @parent_question = create(:parent_question)
+          @compound.questions << @parent_question
+          create_list(:question, 3, parent_question_id: @parent_question.id, generic_page_type_cd: @compound.type_cd, 
+            quizzes_attributes: {
+              "1" => { 'content' => "選択肢１", 'select_correct_flag' => "0" },
+              "2" => { 'content' => "選択肢２", 'select_correct_flag' => "0" },
+              "3" => { 'content' => "選択肢３", 'select_correct_flag' => "0" },
+              "4" => { 'content' => "選択肢４", 'select_correct_flag' => "1" }
+            }
+          )
+          create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+        end
+
+        context '期限終了：合格' do
+          before do
+            create(:passed_answer_score, page_id:@compound.id, user_id: student.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("mark")
+          end
+        end
+
+        context '期限終了：不合格' do
+          before do
+            create(:failed_answer_score, page_id:@compound.id, user_id: student.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("mark")
+          end
+        end
+
+        it "確認＆保存" do
+          create(:failed_answer_score, page_id:@compound.id, user_id: student.id)
+
+          compound_answers = {}
+          @parent_question.questions.each do |question|
+            compound_answers[question.id] = { select_answer_id: question.all_quizzes[0].id }
+          end
+
+          patch :confirm, params: { id: @compound.id, answers: compound_answers }
+          expect(response).to render_template("confirm")
+
+          expect{
+            patch :save, params: { id: @compound.id }
+          }.to change(AnswerScore, :count).by(0)
+
+          @compound.reload
+          count = @compound.answer_score_histories.count
+          expect(count).to eq 1
+          expect(response).to render_template("error")
+        end
+      end
+
+      context '1回受験済み：有効期間内：最大受験回数２回' do
+        before do
+          @compound = create(:compound, course_id: @course.id, start_time: Time.new - 1.day, end_time: Time.new + 1.day, max_count: 2)
+          @parent_question = create(:parent_question)
+          @compound.questions << @parent_question
+          create_list(:question, 3, parent_question_id: @parent_question.id, generic_page_type_cd: @compound.type_cd, 
+            quizzes_attributes: {
+              "1" => { 'content' => "選択肢１", 'select_correct_flag' => "0" },
+              "2" => { 'content' => "選択肢２", 'select_correct_flag' => "0" },
+              "3" => { 'content' => "選択肢３", 'select_correct_flag' => "0" },
+              "4" => { 'content' => "選択肢４", 'select_correct_flag' => "1" }
+            }
+          )
+          create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @compound.id)
+        end
+
+        context '期限終了：合格' do
+          before do
+            create(:passed_answer_score, page_id:@compound.id, user_id: student.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("mark")
+          end
+        end
+
+        context '期限終了：不合格' do
+          before do
+            create(:failed_answer_score, page_id:@compound.id, user_id: student.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @compound.id, class_session_no: @class_session_no }
+
+            expect(response).to render_template("show")
+          end
+        end
+
+        it "確認＆保存" do
+          create(:failed_answer_score, page_id:@compound.id, user_id: student.id)
+
+          compound_answers = {}
+          @parent_question.questions.each do |question|
+            compound_answers[question.id] = { select_answer_id: question.all_quizzes[0].id }
+          end
+
+          patch :confirm, params: { id: @compound.id, answers: compound_answers }
+          expect(response).to render_template("confirm")
+
+          expect{
+            patch :save, params: { id: @compound.id }
+          }.to change(AnswerScore, :count).by(0)
+
+          @compound.reload
+          count = @compound.answer_score_histories.count
+          expect(count).to eq 2
+          expect(response).to redirect_to action: :mark, id: @compound.id
+        end
+      end
+    end
+  end
+end
---
+++ spec/controllers/essays_controller_spec.rb
@@ -0,0 +1,216 @@
+require 'rails_helper'
+
+RSpec.describe EssaysController, type: :controller do
+  let(:student) { create(:student_user) }
+
+  before do
+    @course = create(:course, :year_of_2019)
+
+    @class_session_no = 2
+
+    @class_session = create(:class_session, course_id: @course.id, class_session_no: @class_session_no)
+  end
+
+  describe 'レポート' do
+    context '生徒ユーザ' do
+      before do
+        sign_in student
+
+        create(:course_enrollment_user, course_id: @course.id, user_id: student.id)
+      end
+
+      context '未提出' do
+        context '期限前' do
+          before do
+            @essay = create(:essay, course_id: @course.id, start_time: Time.new + 1.day, end_time: Time.new + 2.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @essay.id }
+
+            expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_BEFORE_START
+            expect(response).to render_template("show")
+          end
+        end
+
+        context '期限終了' do
+          before do
+            @essay = create(:essay, course_id: @course.id, start_time: Time.new - 2.day, end_time: Time.new - 1.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @essay.id }
+
+            expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_AFTER_END
+            expect(response).to render_template("show")
+          end
+        end
+
+        context '有効期間内' do
+          before do
+            @essay = create(:essay, course_id: @course.id, start_time: Time.new - 2.day, end_time: Time.new + 1.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @essay.id }
+
+            expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_NOTPRESENT
+            expect(response).to render_template("show")
+          end
+        end
+
+        context 'パスワード有り' do
+          before do
+            @essay = create(:essay, course_id: @course.id, start_time: Time.new - 2.day, end_time: Time.new + 1.day, start_pass: "12345")
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @essay.id }
+
+            expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_NOTPRESENT
+            expect(response).to render_template("password")
+          end
+
+          it "パスワード誤り" do
+            post :password, params: { id: @essay.id, class_session_no: @class_session_no, start_pass: "11111" }
+
+            expect(response).to render_template("password")
+          end
+
+          it "パスワード一致" do
+            post :password, params: { id: @essay.id, class_session_no: @class_session_no, start_pass: "12345" }
+
+            expect(response).to redirect_to essays_path(@essay)
+          end
+        end
+      end
+
+      context '提出済み' do
+        context '期限終了' do
+          before do
+            @essay = create(:essay, course_id: @course.id, start_time: Time.new - 2.day, end_time: Time.new - 1.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+          end
+
+          it "表示" do
+            get :show, params: { id: @essay.id }
+
+            expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_AFTER_END
+            expect(response).to render_template("show")
+          end
+        end
+      end
+
+      context '提出' do
+        context '期限内' do
+          before do
+            @essay = create(:essay, course_id: @course.id, start_time: Time.new - 2.day, end_time: Time.new + 1.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+          end
+  
+          it "アップロード成功" do
+            post :upload, params: { id: @essay.id, file: fixture_file_upload('test.txt', 'text/txt') }
+
+            expect(response).to redirect_to essays_path(@essay)
+          end
+        end
+  
+        context '期限終了' do
+          before do
+            @essay = create(:essay, course_id: @course.id, start_time: Time.new - 2.day, end_time: Time.new - 1.day)
+            create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+          end
+  
+          it "アップロードエラー" do
+            post :upload, params: { id: @essay.id, file: fixture_file_upload('test.txt', 'text/txt') }
+  
+            expect(response).to render_template("error")
+          end
+        end
+      end
+  
+      context '受理済み：返却の方法ー返却しない' do
+        before do
+          @essay = create(:essay, course_id: @course.id, start_time: Time.new - 2.day, end_time: Time.new + 1.day)
+          create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+        end
+
+        it "採点済（再提出）" do
+          # アップロード
+          post :upload, params: { id: @essay.id, file: fixture_file_upload('test.txt', 'text/txt') }
+          answer_score = @course.essays.first.answer_scores.first
+          answer_score.total_score = 30
+          answer_score.pass_cd = Settings.ANSWERSCORE_PASSCD_UNSUBMITTING
+          answer_score.save!(validate: false)
+
+          get :show, params: { id: @essay.id }
+
+          expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_GRADED_REPRESENT
+          expect(assigns(:is_can_submit)).to eq true
+          expect(response).to render_template("show")
+        end
+
+        it "採点済（受理）" do
+          # アップロード
+          post :upload, params: { id: @essay.id, file: fixture_file_upload('test.txt', 'text/txt') }
+          answer_score = @essay.answer_scores.first
+          answer_score.total_score = 100
+          answer_score.pass_cd = Settings.ANSWERSCORE_PASSCD_SUBMITTED
+          answer_score.save(validate: false)
+      
+          get :show, params: { id: @essay.id }
+
+          expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_GRADED_ACCEPTANCE
+          expect(assigns(:is_can_submit)).to eq false
+          expect(response).to render_template("show")
+        end
+      end
+  
+      context '受理済み：返却の方法ー個別に遂次返却' do
+        before do
+          @essay = create(:essay, course_id: @course.id, start_time: Time.new - 2.day, end_time: Time.new + 1.day,
+            assignment_essay_return_method_cd: Settings.GENERICPAGE_RETURN_METHOD_INDIVIDUALLY)
+          create(:generic_page_class_session_association, class_session_id: @class_session.id, generic_page_id: @essay.id)
+        end
+
+        it "採点済（受理）" do
+          # アップロード
+          post :upload, params: { id: @essay.id, file: fixture_file_upload('test.txt', 'text/txt') }
+          answer_score = @course.essays.first.answer_scores.first
+          answer_score.total_score = 100
+          answer_score.pass_cd = Settings.ANSWERSCORE_PASSCD_SUBMITTED
+          answer_score.save!(validate: false)
+          answer_score.latest_comment.return_flag = Settings.ASSIGNMENTESSAYMAILCOMMENT_RETURNFLG_NOTRETURN
+          answer_score.latest_comment.save!
+
+          get :show, params: { id: @essay.id }
+
+          expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_GRADED_ACCEPTANCE_FEEDBACK
+          expect(assigns(:is_can_submit)).to eq false
+          expect(response).to render_template("show")
+        end
+
+        it "返却済（受理）" do
+          # アップロード
+          post :upload, params: { id: @essay.id, file: fixture_file_upload('test.txt', 'text/txt') }
+          answer_score = @essay.answer_scores.first
+          answer_score.total_score = 100
+          answer_score.pass_cd = Settings.ANSWERSCORE_PASSCD_SUBMITTED
+          answer_score.save(validate: false)
+          answer_score.latest_comment.return_flag = Settings.ASSIGNMENTESSAYMAILCOMMENT_RETURNFLG_RETURNED
+          answer_score.latest_comment.save!
+      
+          get :show, params: { id: @essay.id }
+
+          expect(assigns(:assignmentEssayStatus)).to eq AssignmentEssay::STATUS_RETURNED_ACCEPTANCE_FEEDBACK
+          expect(assigns(:is_can_submit)).to eq false
+          expect(response).to render_template("show")
+        end
+      end
+    end
+  end
+end
