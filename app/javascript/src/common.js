d425699cb61c69863297869765d56448d32bdeb8
Takahiro OKUDA <okuda@bownet.co.jp>
2020-12-27 10:54:43 +0900

    Issue #215

--- app/javascript/src/common.js
+++ app/javascript/src/common.js
@@ -1,8 +1,6 @@
 $(function(){
   $(document).on('change','input[type="file"]', function(){
     var file = this.files[0];
-    console.log(file)
-    console.log(this)
     $(this).parents('label').nextAll('.form-file-name').text(file.name);
   });
 });
